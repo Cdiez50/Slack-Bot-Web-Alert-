5a218f45224f004c066f3879cc82680afca1e191
Cdiez <c.deret@hotmail.fr>
2017-05-30 15:13:06 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+#!/bin/python3.6
+import requests
+import time
+from slackclient import SlackClient
+
+slack_token = "YOUR_TOKEN_HERE"
+sc = SlackClient(slack_token)
+
+urls=['site1', 'site2', 'site3']
+
+while True :
+
+    for i in urls :
+        r = requests.head(i)
+        t = requests.get(i, timeout=5).elapsed.total_seconds()
+        # print(i, r.status_code, t)
+        time.sleep(2)
+
+        if r.status_code != 200 :
+            sc.api_call(
+            "chat.postMessage",
+            channel="#infra",
+            text=("ALERTE !", i, r.status_code)
+            )
+
+        if t > 2 :
+            sc.api_call(
+            "chat.postMessage",
+            channel="#infra",
+            text=("LoadSite too long : ", i, r.status_code)
+            )
+
+    time.sleep(10)
